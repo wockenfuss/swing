a329a37c3b8991e778ab36a5f793668a19cd0a5a
Brent Coughenour <wockenfuss@brentcoughenour.com>
2013-01-31 15:04:12 -0800

    added yaml coli database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,22 @@ class ApplicationController < ActionController::Base
   	"lib/assets/keys/static_map_key.txt"
   end
 
+  def col_path
+  	"lib/assets/keys/wa_key.txt"
+  end
+
   def request_ip
-		  if Rails.env.development? 
-		     response = HTTParty.get('http://api.hostip.info/get_html.php')
-		     ip = response.split("\n")
-		     ip.last.gsub /IP:\s+/, ''      
-		   else
-		     request.remote_ip
-		   end 
-		end
+	  if Rails.env.development? 
+	     response = HTTParty.get('http://api.hostip.info/get_html.php')
+	     ip = response.split("\n")
+	     ip.last.gsub /IP:\s+/, ''      
+	   else
+	     request.remote_ip
+	   end 
+	end
+
+	def col_index
+		index ||= YAML::load(File.open('lib/coli.yaml'))
+	end
   
 end
