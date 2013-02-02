3ff2f31b51f0c2b331e0f907b4450a8a2a84d690
Brent Coughenour <wockenfuss@brentcoughenour.com>
2013-02-02 15:05:48 -0800

    changed user-location association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,10 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, 
+  								:remember_me
 
-  has_and_belongs_to_many :locations
+  belongs_to :home_location, :class_name => "Location"
 
 end
