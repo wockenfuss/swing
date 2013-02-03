5f114085dacc49cb6a0673dadba3e94f36aaf6b5
Brent Coughenour <wockenfuss@brentcoughenour.com>
2013-02-02 18:42:07 -0800

    modified return format for module

--- lib/geography.rb
+++ lib/geography.rb
@@ -2,7 +2,7 @@ module Geography
 
 	def self.city_state(ip_address)
 		result = Geocoder.search(ip_address)[0].data	
-		return [result["city"], result["region_code"]]
+		return "#{result["city"]} #{result["region_code"]}"
 	end	
 
 end
