48fa08c6ccc3540166515a879ae865b130fbdc7e
Brent Coughenour <wockenfuss@brentcoughenour.com>
2013-02-04 10:32:37 -0800

    added default location if ip call fails

--- lib/geography.rb
+++ lib/geography.rb
@@ -1,9 +1,13 @@
 module Geography
 
 	def self.city_from_ip(ip_address)
-		result = Geocoder.search(ip_address)[0].data	
-		# result = Geocoder.search(208.113.83.165)[0].data
-		return "#{result["city"]} #{result["region_code"]}"
+		result = Geocoder.search(ip_address)
+		if result != []
+			return "#{result[0].data["city"]} #{result[0].data["region_code"]}"
+			# result = Geocoder.search("208.113.83.165")[0].data
+		else
+			return "San Francisco CA"
+		end
 	end	
 
 	def self.city_from_city(city)
