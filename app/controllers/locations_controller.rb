3158ec9d39485681c0bc4269dae280f888a6f55f
Brent Coughenour <wockenfuss@brentcoughenour.com>
2013-02-06 14:44:40 -0800

    changed locations controller for salary parameter

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -6,16 +6,18 @@ def show
 		if params[:city] && params[:city] != ""
 			location = Location.from_ip_or_city(params[:city])
 		else
-			location = @user.home_location || Location.from_ip_or_city
+			location = @user.location || Location.from_ip_or_city
 		end
 
 		key = File.read(static_map_path)
 		cost_index = location.cost_index if location
+		salary = @user.salary
 		respond_to do |format|
 			format.html
 			format.json { render :json => { :location => location,
 																			:key => key,
-																			:cost_index => cost_index
+																			:cost_index => cost_index,
+																			:salary => salary
 																		}}
 		end
 
